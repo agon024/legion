f8edfe82c13f8aaa8579ce2e786839d43822fab3
Kevin Ray <agon024@gmail.com>
2017-03-08 14:15:09 -0800

    removed useless files

--- contact_2.php
+++ contact_2.php
@@ -16,7 +16,7 @@
 
     // Set the recipient email address.
     // FIXME: Update this to your desired email address.
-    $recipient = "kevin@legiondesigns.com";
+    $recipient = "my@email.com";
 
     // Set the email subject.
     $subject = trim($_POST["subject"]);
